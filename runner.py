55029456c3a7e7c946793836c8eda77883fd3b65
Temesghen Kahsai <lememta@gmail.com>
2016-01-29 10:31:42 -0800

    more stuff

--- runner.py
+++ runner.py
@@ -5,6 +5,7 @@ import csv
 import os
 import glob
 import subprocess
+import stats
 
 def processFile(bench, result, tool):
     stats = {"tool": tool, "ans":"", "time":"", "mem":"", "inst":""}
@@ -14,6 +15,7 @@ def processFile(bench, result, tool):
     if tool == "JPF":
         for r in result.splitlines():
             if 'no errors detected' in r:
+                #stat('Result-JPF', 'SAFE')
                 stats.update({"ans":"SAFE"})
             if 'elapsed time' in r:
                 t = r.split()
@@ -28,11 +30,16 @@ def processFile(bench, result, tool):
                 ins = t[len(t)-1]
                 stats.update({"inst":str(ins)})
             if 'java.lang.AssertionError' in r:
+                #stat('Result-JPF', 'CEX')
                 stats.update({"ans":"CEX"})
-    elif tool == "JAYHORN":
+    elif tool == "Z3" or tool == "ELD":
         if "checker says true" in result:
+            #if tool == "Z3": stat('Result-Z3', 'SAFE')
+            #if tool == "ELD": stat('Result-ELDARICA', 'SAFE')
             stats.update({"ans":"SAFE"})
         if "checker says false" in result:
+            #if tool == "Z3": stat('Result-Z3', 'CEX')
+            #if tool == "ELD": stat('Result-ELDARICA', 'CEX')
             stats.update({"ans":"CEX"})
     return {bench:stats}, stats
 
@@ -42,17 +49,66 @@ config = """ target=Main
 """
 
 JPF = "./jpf-travis/jpf-core/build/RunJPF.jar"
+
 JAYHORN = "./jayhorn/jayhorn/build/libs/jayhorn.jar"
 
 
 def runJar(jar):
     try:
+        #with stats.timer(tool):
         p = subprocess.Popen(jar, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         result, _ = p.communicate()
+        #stats.stop(tool)
         return result
     except Exception as e:
         print str(e)
         return None
+
+
+class BenchStats(object):
+    def __init__(self, tools, bench):
+        self.tools = tools
+        self.t_n = tool_name
+        result = 'Result-'+str(tool_name)
+        self.stat(result, 'UNKNOWN')
+        self.stat('BENCHMARK', str(bench))
+        return
+
+    def run(self):
+        result = self.runJar()
+        self.processFile(result)
+        stats.brunch_print()
+
+
+    def runJar(self):
+        try:
+            tt = 'TIME-'+str(self.t_n)
+            with stats.timer(tt):
+                p = subprocess.Popen(self.t_e, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                result, _ = p.communicate()
+                return result
+
+        except Exception as e:
+            print str(e)
+            return None
+
+    def stat (self, key, val): stats.put (key, val)
+
+    def processFile(self, result):
+        if self.t_n == "JPF":
+            for r in result.splitlines():
+                if 'no errors detected' in r:
+                    self.stat('Result-JPF', 'SAFE')
+                if 'java.lang.AssertionError' in r:
+                    self.stat('Result-JPF', 'CEX')
+        elif self.t_n == "Z3" or self.t_n == "ELDARICA":
+            if "checker says true" in result:
+                if self.t_n == "Z3": self.stat('Result-Z3', 'SAFE')
+                if self.t_n == "ELDARICA": self.stat('Result-ELDARICA', 'SAFE')
+            if "checker says false" in result:
+                if self.t_n == "Z3": self.stat('Result-Z3', 'CEX')
+                if self.t_n == "ELDARICA": self.stat('Result-ELDARICA', 'CEX')
+
 
 
 def runBench(args):
@@ -62,15 +118,16 @@ def runBench(args):
     all_results = {}
     for d in all_dir:
         print "Benchmark:\t " + str(d)
+        #stat('BENCHMARK', str(d))
         tmp = d.split("/")
         bench = tmp[len(tmp)-1]
         jpf = glob.glob(os.path.abspath(d) + os.sep + "*.jpf")
-        cmd_z3 = ['java', "-jar", JAYHORN, "-solver", "z3",  "-t", "60", "-j", d]
-        cmd_eldarica = ['java', "-jar", JAYHORN, "-j", d]
+        cmd_z3 = ['java', "-jar", JAYHORN, "-solver", "z3",  "-t", "20", "-j", d]
+        cmd_eldarica = ['java', "-jar", JAYHORN, "-t", "20", "-j", d]
         z3_result = runJar(cmd_z3)
-        z3_ans, z3_stats = processFile(bench, z3_result, "JAYHORN")
+        z3_ans, z3_stats = processFile(bench, z3_result, "Z3")
         eldarica_result = runJar(cmd_eldarica)
-        eldarica_ans, eldarica_stats = processFile(bench, eldarica_result, "JAYHORN")
+        eldarica_ans, eldarica_stats = processFile(bench, eldarica_result, "ELD")
         if len(jpf) == 1:
             # file = fileinput.FileInput(jpf[0], inplace=True, backup='.bak')
             # for line in file:
@@ -84,12 +141,27 @@ def runBench(args):
             print "JPF RESULT:\t" + "NO JPF CONFIG"
         print "JAYHORN (ELDARICA) RESULT:\t" + str(eldarica_stats)
         print "JAYHORN (Z3) RESULT:\t" + str(z3_stats)
+        #stats.brunch_print()
 
         print "---------------------"
 
-            #all_results.update(ans)
-    # print "---- SUMMARY ----"
-    # print all_results
+
+# def runBench(args):
+#     dr = args.directory
+#     all_dir = [os.path.join(dr, name)for name in os.listdir(dr) if os.path.isdir(os.path.join(dr, name)) ]
+#     all_results = {}
+#     for d in all_dir:
+#         tmp = d.split("/")
+#         bench = tmp[len(tmp)-1]
+#         jpf = glob.glob(os.path.abspath(d) + os.sep + "*.jpf")
+#         cmd_z3 = ['java', "-jar", JAYHORN, "-solver", "z3",  "-t", "20", "-j", d]
+#         cmd_eldarica = ['java', "-jar", JAYHORN, "-t", "20", "-j", d]
+#         cmd_jpf = ['java', "-jar", JPF, "+shell.port=4242", jpf[0]] if len(jpf) == 1 else []
+#         tools = {"Z3", cmd_z3, "ELDARICA", cmd_eldarica, "JPF", cmd_jpf}
+#         runner = BenchStats(tools, d)
+#         runner.run()
+
+
 
 
 if __name__ == "__main__":
