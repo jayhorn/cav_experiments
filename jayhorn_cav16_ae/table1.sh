c0799b0656f00f079af8e6ba7c307e52383c7f6d
Teme <lememta@gmail.com>
2016-05-17 11:51:14 -0700

    mofified

--- jayhorn_cav16_ae/table1.sh
+++ jayhorn_cav16_ae/table1.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-cd benchmarks
+cd benchmarks/cbmc
 
 echo "Running the CBMC benchmarks"
-for file in */ ; do 
+for file in */ ; do
   if [[ -d "$file" && ! -L "$file" ]]; then
-    echo "Running CBMC benchmark $file"; 
-    time java -jar ../jayhorn.jar -j $file
-  fi; 
+    echo "Running CBMC benchmark $file";
+    time java -jar ../../jayhorn.jar -j $file -t 60
+  fi;
 done
 
 cd ../svcomp_rec
 echo "Running the SVCOMP benchmarks"
-for file in */ ; do 
+for file in */ ; do
   if [[ -d "$file" && ! -L "$file" ]]; then
-    echo "Running SVCOMP benchmark $file"; 
-    time java -jar ../jayhorn.jar -j $file
-  fi; 
+    echo "Running SVCOMP benchmark $file";
+    time java -jar ../../jayhorn.jar -j $file -t 60
+  fi;
 done
 
 cd ../MinePump/spec1-5/
 echo "Running the MinePump benchmarks"
-for file in */ ; do 
+for file in */ ; do
   if [[ -d "$file" && ! -L "$file" ]]; then
-    echo "Running MinePump benchmark $file"; 
-    time java -jar ../jayhorn.jar -j $file
-  fi; 
+    echo "Running MinePump benchmark $file";
+    time java -jar ../../../jayhorn.jar -j $file -t 60
+  fi;
 done
--- jayhorn_cav16_ae/table1_z3.sh
+++ jayhorn_cav16_ae/table1_z3.sh
@@ -3,30 +3,31 @@
 export LD_LIBRARY_PATH=./native_lib
 export DYLD_LIBRARY_PATH=./native_lib
 
-cd benchmarks
+cd benchmarks/cbmc
 
 echo "Running the CBMC benchmarks"
-for file in */ ; do 
+for file in */ ; do
   if [[ -d "$file" && ! -L "$file" ]]; then
-    echo "Running CBMC benchmark $file"; 
-    time java -jar ../jayhorn.jar —solver z3 -j $file
-  fi; 
+    echo "Running CBMC benchmark $file";
+    time java -jar ../../jayhorn.jar -j $file -solver z3 -t 60
+  fi;
 done
+
 
 cd ../svcomp_rec
 echo "Running the SVCOMP benchmarks"
-for file in */ ; do 
-  if [[ -d "$file" && ! -L "$file" ]]; then
-    echo "Running SVCOMP benchmark $file"; 
-    time java -jar ../jayhorn.jar —solver z3 -j $file
-  fi; 
+for file in */ ; do
+    if [[ -d "$file" && ! -L "$file" ]]; then
+        echo "Running SVCOMP benchmark $file";
+        time java -jar ../../jayhorn.jar -j $file -solver z3 -t 60
+    fi;
 done
 
 cd ../MinePump/spec1-5/
 echo "Running the MinePump benchmarks"
-for file in */ ; do 
-  if [[ -d "$file" && ! -L "$file" ]]; then
-    echo "Running MinePump benchmark $file"; 
-    time java -jar ../jayhorn.jar —solver z3 -j $file
-  fi; 
+for file in */ ; do
+    if [[ -d "$file" && ! -L "$file" ]]; then
+        echo "Running MinePump benchmark $file";
+        time java -jar ../../../jayhorn.jar -j $file -solver z3 -t 60
+    fi;
 done
